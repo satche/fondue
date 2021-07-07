ca399beb548bcbca1809ab4381248c1065e8741a
Satche <info@satche.ch>
2021-07-07 18:18:40 +0200

    Update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -4,8 +4,21 @@ module.exports = {
 			"postcss-preset-env",
 			{
 				browsers: "last 2 versions",
-				stage: 1
-			}
-		]
-	]
-}
+
+				// See more at: https://cssdb.org/
+				stage: 3,
+				features: {
+					"nesting-rules": true,
+					"custom-selectors": true,
+				},
+				// importFrom: "src/css/settings/custom-selectors.css",
+			},
+		],
+		[
+			"postcss-mixins",
+			{
+				// mixinsFiles: "src/css/settings/mixins.css",
+			},
+		],
+	],
+};
